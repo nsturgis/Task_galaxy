94935297a40e3a2f117519fcbbb876f57a55454a
Helen Hood <helen.c.hood@gmail.com>
2014-09-16 10:52:48 -0400

    Add validation of name to TaskList

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -2,4 +2,6 @@ class TaskList < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :user_task_lists
   has_many :users, through: :user_task_lists
+
+  validates :name, presence: true
 end
