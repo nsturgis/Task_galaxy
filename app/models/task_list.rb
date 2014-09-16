86140f86cf78e3b0714238cb5ca7956071635dad
Helen Hood <helen.c.hood@gmail.com>
2014-09-16 10:40:05 -0400

    Rename & edit TaskList class

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,6 +1,6 @@
-class Task_list < ActiveRecord::Base
-  has_many :users
+class TaskList < ActiveRecord::Base
+  # has_many :users - users should have many TaskLists,
+  # in which case you'd have to use a join table and do this association
+  # a bit differently
   has_many :tasks, dependent: :destroy
-
 end
-
